3be39a04b4dd8db8abc34d08654d00dd18647568
Collins Pamba <collinspamba@outlook.com>
2026-02-25 12:25:18 +0300

    minor fix

--- src/Web/WebPlatform.php
+++ src/Web/WebPlatform.php
@@ -1,13 +1,5 @@
 <?php
 
-/* 
-Copyright (C) 2026 Collins Pamba
-
-This file is part of MaweTatu and MaweTatu is free software:
-you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation,
-either version 3 of the License, or (at your option) any later version.
-*/
-
 declare(strict_types= 1);
 
 namespace Marshal\Platform\Web;
